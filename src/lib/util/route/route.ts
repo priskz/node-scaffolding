d412465788d71ce2327876ad07a577897f620330
Zachary Prisk <zachary.prisk@gmail.com>
2020-06-04 22:37:52 -0500

    Implements concept of optional "after" middleware into route util

--- src/lib/util/route/route.ts
+++ src/lib/util/route/route.ts
@@ -2,6 +2,48 @@ import { Router, RequestHandler } from 'express'
 import asyncHandler from 'express-async-handler'
 
 /*
+ * Parse given request handlers into Async Middleware
+ */
+function _parseAsyncMiddleware(
+	handler: RequestHandler | RequestHandler[] | undefined
+): RequestHandler[] {
+	// Wrapped middleware
+	let middleware: RequestHandler[] = []
+
+	// Middleware not defined?
+	if (Array.isArray(handler)) {
+		// Add single mw to var
+		middleware = handler as RequestHandler[]
+	}
+
+	// Single middleware?
+	if (handler && !Array.isArray(handler)) {
+		middleware = [handler] as RequestHandler[]
+	}
+
+	// Convert all middleware to async, so exceptions are caught
+	for (let x = 0; x < middleware.length; x++) {
+		middleware[x] = asyncHandler(middleware[x])
+	}
+
+	// Return
+	return middleware
+}
+
+/*
+ * Parse given handler into async Handler
+ */
+function _parseHandler(handler: RequestHandler, sync = false): RequestHandler {
+	// Async or sync?
+	if (!sync) {
+		// Convert to async
+		handler = asyncHandler(handler)
+	}
+
+	return handler
+}
+
+/*
  * Register routes into the given router
  */
 function register(
@@ -17,37 +59,18 @@ function register(
 	// Iterate routes
 	for (let i = 0; i < routes.length; i++) {
 		// Wrapped middleware
-		let middleware: RequestHandler[] = []
+		let middleware = _parseAsyncMiddleware(routes[i].middleware)
 
-		// Middleware not defined?
-		if (Array.isArray(routes[i].middleware)) {
-			// Add single mw to var
-			middleware = routes[i].middleware as RequestHandler[]
-		}
+		// Wrapped after middleware
+		let after = _parseAsyncMiddleware(routes[i].after)
 
-		// Single middleware?
-		if (routes[i].middleware && !Array.isArray(routes[i].middleware)) {
-			middleware = [routes[i].middleware] as RequestHandler[]
-		}
-
-		// Convert all middleware to async, so any MW exceptions are caught
-		for (let x = 0; x < middleware.length; x++) {
-			middleware[x] = asyncHandler(middleware[x])
-		}
-
-		// Set route handler
-		let handler = routes[i].handler
-
-		// Async or sync?
-		if (routes[i].sync !== true) {
-			// Convert to async
-			handler = asyncHandler(routes[i].handler)
-		}
+		// Wrapped after middleware
+		let handler = _parseHandler(routes[i].handler, routes[i].sync)
 
 		// Register route
 		router
 			.route(`${base}${routes[i].path}`)
-			[routes[i].method](middleware, handler)
+			[routes[i].method](middleware, handler, after)
 	}
 }
 
@@ -72,4 +95,5 @@ export interface RouteConfig {
 	handler: RequestHandler
 	middleware?: RequestHandler | RequestHandler[] | undefined
 	sync?: boolean
+	after?: RequestHandler | RequestHandler[] | undefined
 }
