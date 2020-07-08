e256f6ddb99adff9aacf0b1164007967d8ca7e24
Zachary Prisk <zachary.prisk@gmail.com>
2020-07-08 01:40:32 -0500

    Implements functionality and limited testing for SessionRoot service

--- src/app/service/root/session/SessionRoot.spec.ts
+++ src/app/service/root/session/SessionRoot.spec.ts
@@ -1,9 +1,103 @@
+import { expect } from 'chai'
 import { MockSession } from '~/test/mocks'
+import { Session } from '~/app/domain'
+import { SessionRoot } from './'
 
-describe.skip('app/service/root/session/SessionRoot', () => {
-	describe('todo', () => {
-		it('todo', async () => {
-			//
+describe('app/service/root/session/SessionRoot', () => {
+	// Unit
+	let service: SessionRoot
+
+	// Mock Session
+	let mockSession: Session
+
+	before(async () => {
+		// Clean up
+		mockSession = await MockSession.create()
+	})
+
+	after(async () => {
+		// Clean up
+		await MockSession.destroy(mockSession.id)
+	})
+
+	describe('constructor method', () => {
+		it('should return new instance of SessionRoot', async () => {
+			// Test
+			service = new SessionRoot()
+
+			// Assertions
+			expect(service).to.be.an.instanceOf(SessionRoot)
+		})
+	})
+
+	describe('getOneById method', () => {
+		it('if found should return Session', async () => {
+			// Test
+			const result = await service.getOneById(mockSession.id)
+
+			// Assertions
+			expect(result)
+				.to.have.property('id')
+				.to.equal(mockSession.id)
+		})
+
+		it('if NOT found should return undefined', async () => {
+			// Test
+			const reuslt = await service.getOneById('id-does-not-exist-in-db')
+
+			// Assertions
+			expect(reuslt).to.be.undefined
+		})
+	})
+
+	describe.skip('updateActiveAt method', () => {
+		it('shoudl update Session activateAt and return void', async () => {
+			// // Test
+			// const result = await service.getOneById(mockSession.id)
+			// // Assertions
+			// expect(result)
+			// 	.to.have.property('id')
+			// 	.to.equal(mockSession.id)
+		})
+
+		it('failure to update should return false', async () => {
+			// // Test
+			// const result = await service.getOneById(mockSession.id)
+			// // Assertions
+			// expect(result)
+			// 	.to.have.property('id')
+			// 	.to.equal(mockSession.id)
+		})
+	})
+
+	describe.skip('expire method', () => {
+		it('success update should return true', async () => {
+			// // Test
+			// const result = await service.getOneById(mockSession.id)
+			// // Assertions
+			// expect(result)
+			// 	.to.have.property('id')
+			// 	.to.equal(mockSession.id)
+		})
+
+		it('failure to update should return false', async () => {
+			// // Test
+			// const result = await service.getOneById(mockSession.id)
+			// // Assertions
+			// expect(result)
+			// 	.to.have.property('id')
+			// 	.to.equal(mockSession.id)
+		})
+	})
+
+	describe.skip('attachUser method', () => {
+		it('success should return Session with userId populated', async () => {
+			// // Test
+			// const result = await service.getOneById(mockSession.id)
+			// // Assertions
+			// expect(result)
+			// 	.to.have.property('id')
+			// 	.to.equal(mockSession.id)
 		})
 	})
 })
--- src/app/service/root/session/SessionRoot.ts
+++ src/app/service/root/session/SessionRoot.ts
@@ -1,4 +1,6 @@
-import { User } from '~/app/domain'
+import moment from 'moment'
+import { config } from '~/config'
+import { Session, User } from '~/app/domain'
 import { SessionService, UserService } from '~/app/service/data'
 
 export class SessionRoot {
@@ -15,10 +17,55 @@ export class SessionRoot {
 	}
 
 	/*
-	 *
+	 * Generate a new Session
 	 */
-	public async attachUser(sessionId: string): Promise<Boolean> {
-		// Expire session
-		return await this.session.expire(sessionId)
+	public async generate(
+		browserAgent: string,
+		ipAddress: string
+	): Promise<Session | undefined> {
+		// TODO: Clean up time logic
+		// Current time
+		const now = new Date()
+
+		// Create expiresAt
+		const expiresAt = new Date(now)
+
+		// Add session duration to value
+		expiresAt.setDate(now.getDate() + config.session.duration.guest)
+
+		// Create
+		return await this.session.create({
+			browserAgent,
+			ipAddress,
+			expiresAt
+		})
+	}
+
+	/*
+	 * Get a single Session by id
+	 */
+	public async getOneById(id: string): Promise<Session | undefined> {
+		return await this.session.getOne({ where: { id } })
+	}
+
+	/*
+	 * Update activeAt on Session
+	 */
+	public async updateActiveAt(id: string): Promise<void> {
+		await this.session.update({ id, activeAt: moment().toDate() })
+	}
+
+	/*
+	 * Expire a Session
+	 */
+	public async expire(id: string): Promise<boolean> {
+		return !!(await this.session.update({ id, expiresAt: moment().toDate() }))
+	}
+
+	/*
+	 * Attach User to a Session
+	 */
+	public async attachUser(sessionId: string): Promise<any> {
+		// TODO:
 	}
 }
