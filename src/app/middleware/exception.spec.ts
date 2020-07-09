6b6556290d15a9be9a4002ee12cc4936449c70b7
Zachary Prisk <zachary.prisk@gmail.com>
2020-07-09 00:23:49 -0500

    Adds testing for global exception middleware

--- src/app/middleware/exception.spec.ts
+++ src/app/middleware/exception.spec.ts
@@ -1,12 +1,39 @@
-import moment from 'moment'
+import { expect } from 'chai'
 import { AxiosResponse } from 'axios'
+import { appRequest } from '~/test/util'
 
 //----- Tests -----//
 
 describe('middleware/exception', () => {
-	describe.skip('when an error is thrown', () => {
-		it('should log thee rror', async () => {
-			//
+	describe('error is thrown', () => {
+		it('should catch error, log, handle gracefully, and return 500', async () => {
+			// Test
+			const result: AxiosResponse = await appRequest.post('/error')
+
+			// Assertions
+			expect(result.status).to.equal(500)
+		})
+	})
+
+	describe('error is thrown while env DEBUG_MODE true', () => {
+		it('should return 500 with detail in body', async () => {
+			// Test
+			const result: AxiosResponse = await appRequest.post('/error')
+
+			// Assertions
+			expect(result.status).to.equal(500)
+			expect(result.data).to.not.be.empty
+		})
+	})
+
+	describe.skip('error is thrown while env DEBUG_MODE false', () => {
+		it('should return 500 with empty body', async () => {
+			// Test
+			const result: AxiosResponse = await appRequest.post('/error')
+
+			// Assertions
+			expect(result.status).to.equal(500)
+			expect(result.data).to.be.empty
 		})
 	})
 })
