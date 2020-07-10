0347c13bc08686db66b486456fdfbc353f716201
Zachary Prisk <zachary.prisk@gmail.com>
2020-07-09 20:38:14 -0500

    Cleans up database config file

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,47 +1,70 @@
 import { ConnectionOptions } from 'typeorm'
-import { env } from '~/lib/util'
 
-const prod = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === 'production'
 
-export const db: DbConfig = {
-	type: env('TYPEORM_CONNECTION') as ConnectionType,
-	host: env('TYPEORM_HOST'),
-	port: env('TYPEORM_PORT', 3306, 'int'),
-	username: env('TYPEORM_USERNAME'),
-	password: env('TYPEORM_PASSWORD'),
-	database: env('TYPEORM_DATABASE'),
-	synchronize: env('TYPEORM_SYNCHRONIZE', false),
-	logging: env('TYPEORM_LOGGING', false),
-	// TODO: Clean Up
-	entities: !prod
-		? env('TYPEORM_ENTITIES', '', 'array')
-		: env('TYPEORM_ENTITIES', '', 'array').map((x: string): string => {
-				return x.replace('ts', 'js').replace('src', 'dist')
-		  }),
-	migrations: !prod
-		? env('TYPEORM_MIGRATIONS', '', 'array')
-		: env('TYPEORM_MIGRATIONS', '', 'array').map((x: string) => {
-				return x.replace('ts', 'js').replace('src', 'dist')
-		  }),
-	subscribers: !prod
-		? env('TYPEORM_SUBSCRIBERS', '', 'array')
-		: env('TYPEORM_SUBSCRIBERS', '', 'array').map((x: string) => {
-				return x.replace('ts', 'js').replace('src', 'dist')
-		  }),
-	cli: {
-		// TODO: Clean Up
-		entitiesDir: !prod
-			? env('TYPEORM_ENTITIES_DIR')
-			: env('TYPEORM_ENTITIES_DIR').replace('src', 'dist'),
-		migrationsDir: !prod
-			? env('TYPEORM_MIGRATIONS_DIR')
-			: env('TYPEORM_MIGRATIONS_DIR').replace('src', 'dist'),
-		subscribersDir: !prod
-			? env('TYPEORM_SUBSCRIBERS_DIR')
-			: env('TYPEORM_SUBSCRIBERS_DIR').replace('src', 'dist')
-	}
+const {
+	TYPEORM_CONNECTION,
+	TYPEORM_HOST,
+	TYPEORM_PORT,
+	TYPEORM_USERNAME,
+	TYPEORM_PASSWORD,
+	TYPEORM_DATABASE,
+	TYPEORM_SYNCHRONIZE,
+	TYPEORM_LOGGING,
+	TYPEORM_ENTITIES,
+	TYPEORM_MIGRATIONS,
+	TYPEORM_SUBSCRIBERS,
+	TYPEORM_ENTITIES_DIR,
+	TYPEORM_MIGRATIONS_DIR,
+	TYPEORM_SUBSCRIBERS_DIR
+} = process.env
+
+if (
+	!TYPEORM_CONNECTION ||
+	!TYPEORM_HOST ||
+	!TYPEORM_PORT ||
+	!TYPEORM_USERNAME ||
+	!TYPEORM_PASSWORD ||
+	!TYPEORM_DATABASE ||
+	!TYPEORM_SYNCHRONIZE ||
+	!TYPEORM_LOGGING ||
+	!TYPEORM_ENTITIES ||
+	!TYPEORM_MIGRATIONS ||
+	!TYPEORM_SUBSCRIBERS ||
+	!TYPEORM_ENTITIES_DIR ||
+	!TYPEORM_MIGRATIONS_DIR ||
+	!TYPEORM_SUBSCRIBERS_DIR
+) {
+	throw new Error('Database misconfiguration')
 }
 
+/**
+ * Replaces development file paths with production file paths if in production mode
+ */
+function getEnvPath(path: string): string {
+	return !production
+		? path
+		: path.replace('.ts', '.js').replace('src/', 'dist/')
+}
+
+export const db: DbConfig = {
+	type: TYPEORM_CONNECTION as ConnectionType,
+	host: TYPEORM_HOST,
+	port: parseInt(TYPEORM_PORT),
+	username: TYPEORM_USERNAME,
+	password: TYPEORM_PASSWORD,
+	database: TYPEORM_DATABASE,
+	synchronize: TYPEORM_SYNCHRONIZE === 'true',
+	logging: TYPEORM_LOGGING === 'true',
+	entities: [getEnvPath(TYPEORM_ENTITIES)],
+	migrations: [getEnvPath(TYPEORM_MIGRATIONS)],
+	subscribers: [getEnvPath(TYPEORM_SUBSCRIBERS)],
+	cli: {
+		entitiesDir: getEnvPath(TYPEORM_ENTITIES_DIR),
+		migrationsDir: getEnvPath(TYPEORM_MIGRATIONS_DIR),
+		subscribersDir: getEnvPath(TYPEORM_SUBSCRIBERS_DIR)
+	}
+}
 export type DbConfig = ConnectionOptions
 
 type ConnectionType =
